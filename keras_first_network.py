6059d8f4cc383e5d04bdaacca2a51aa03db02d2c
Shreyas Choudhary <shreyas.choudhary@mail.utoronto.ca>
2021-04-27 18:17:09 -0400

    First Commit

---
+++ keras_first_network.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Source: https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/ 
+
+# In[1]:
+
+
+# first neural network with keras tutorial
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+# In[2]:
+
+
+# load the dataset
+dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
+# split into input (X) and output (y) variables
+X = dataset[:,0:8]
+y = dataset[:,8]
+
+
+# In[3]:
+
+
+# define the keras model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+
+# In[4]:
+
+
+# compile the keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# In[5]:
+
+
+# fit the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10)
+
+
+# In[6]:
+
+
+# evaluate the keras model
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
+
+
+# In[7]:
+
+
+# make class predictions with the model
+predictions = model.predict_classes(X)
+# summarize the first 5 cases
+for i in range(5):
+	print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
+
+
+# In[ ]:
+
+
+
+
